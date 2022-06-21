c3171ff5782c099a68a01b96d5a33aa872081de6
dk <omnipresent9@yandex.ru>
2022-06-21 16:56:48 +0300

    ДЗ 1

--- app/src/main/java/com/example/myhomework/MainActivity.java
+++ app/src/main/java/com/example/myhomework/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myhomework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button switchActivity1 = findViewById(R.id.switch_to_input);
+        Button switchActivity2 = findViewById(R.id.switch_to_ems);
+
+        switchActivity1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InputTypeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        switchActivity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EmsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
